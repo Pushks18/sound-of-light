37b4e191a1ac854958e0a163e2ec76c3a3bb4679
Praveen <s.praveen@live.in>
2026-02-17 14:58:25 -0800

    changing light behaviour
    Signed-off-by: Praveen <s.praveen@live.in>

--- Assets/Scripts/TimedDestroy.cs
+++ Assets/Scripts/TimedDestroy.cs
@@ -4,28 +4,31 @@ using UnityEngine.Rendering.Universal;
 public class EchoPulse : MonoBehaviour
 {
     private Light2D light2D;
-    public float expandSpeed = 15f;    // 扩散速度
-    public float fadeSpeed = 2f;      // 消失速度
+    public float expandSpeed = 15f;
+    public float fadeSpeed = 2f;
     private float currentIntensity;
+    private float currentRadius;
 
     void Awake()
     {
         light2D = GetComponent<Light2D>();
         currentIntensity = light2D.intensity;
-        // 初始大小设小一点
-        transform.localScale = Vector3.zero;
+        currentRadius = 0f;
+        light2D.pointLightOuterRadius = 0f;
+        light2D.pointLightInnerRadius = 0f;
     }
 
     void Update()
     {
-        // 1. 让圆环向外扩张
-        transform.localScale += Vector3.one * expandSpeed * Time.deltaTime;
+        // Expand the light radius directly
+        currentRadius += expandSpeed * Time.deltaTime;
+        light2D.pointLightOuterRadius = currentRadius;
+        light2D.pointLightInnerRadius = currentRadius * 0.5f;
 
-        // 2. 让亮度逐渐变暗
+        // Fade intensity
         currentIntensity -= fadeSpeed * Time.deltaTime;
         light2D.intensity = currentIntensity;
 
-        // 3. 当完全变黑时，销毁自己
         if (currentIntensity <= 0)
         {
             Destroy(gameObject);
